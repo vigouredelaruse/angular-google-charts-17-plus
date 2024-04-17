f329b272e8766db7e94f1d1547d4f7a5e5fd7904
vigouredelaruse <velvetsheen@hotmail.com>
2024-04-17 01:51:35 -0400

    destination dir bugfix

--- apps/playground/src/app/app.module.ts
+++ apps/playground/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { GoogleChartsModule } from '../../../../libs/angular-google-charts';
+import { GoogleChartsModule } from '@horselessforks/angular-google-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
